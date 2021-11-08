011fa27016d9381026ba99d70f5854401270e1bb
root <root@localhost>
2021-11-08 11:20:12 +0530

    auto publish 1636350611780

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+// Logic at the End of The Program :)
+
+int main()
+{
+    int num;
+    cout << "Enter a Number: " << endl;
+    cin >> num;
+    
+    if (num % 2 == 0){
+      cout << num << " is an Even_Number :)" << endl;  }
+    else{
+      cout << num << " is an Odd_Number :)" << endl;  }    
+    return 0;
+
+}
+
+
+/* 8-Nov-21[11:11am].
+Determining whether a number is Even or Odd:-
+[listening to "Fly me to the Moon" in Background:)]
+
+So what's an odd Number?
+First , a number which isn't Even , is Odd
+
+Whats an Even number?; A number which is com-
+-pletely divisible by 2.
+
+Hence, if a number%2 != 0, its Odd. Else its Even
+-------------Thats the Logic Asshole!--------- */
+//Listening to "Goin' Down the Road Feelin' Bad"
